1acb79cf6b79d0844fa2e127e3369ba6daf3e0d0
Daisuke Maki <lestrrat+github@gmail.com>
2018-03-23 10:05:49 +0900

    Map number+long to int64, and number+integer to int32
    fixes #62

--- proto_test.go
+++ proto_test.go
@@ -256,6 +256,12 @@ func TestGenerateProto(t *testing.T) {
 
 			"fixtures/semantic_api.proto",
 		},
+		{
+			true,
+			false,
+			"fixtures/integers.yaml",
+			"fixtures/integers.proto",
+		},
 	}
 
 	origin, _ := os.Getwd()
