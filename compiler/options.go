cbdf4fd57b4e417147eeb23a5518319b8d9abdcf
DanMaas <daniel.maas@target.com>
2018-12-18 20:28:37 -0600

    allow specification of a map of repeated items, add options to disable generation of rpcs and enable enum prefixes

--- compiler/options.go
+++ compiler/options.go
@@ -3,11 +3,23 @@ package compiler
 import "github.com/NYTimes/openapi2proto/internal/option"
 
 const (
-	optkeyAnnotation = "annotation"
+	optkeyAnnotation  = "annotation"
+	optkeySkipRpcs    = "skip-rpcs"
+	optkeyPrefixEnums = "namespace-enums"
 )
 
 // WithAnnotation creates a new Option to specify if we should add
 // google.api.http annotation to the compiled Protocol Buffers structure
 func WithAnnotation(b bool) Option {
 	return option.New(optkeyAnnotation, b)
+}
+
+// WithSkipRpcs creates a new Option to specify if we should
+// generate services and rpcs in addition to messages
+func WithSkipRpcs(b bool) Option {
+	return option.New(optkeySkipRpcs, b)
+}
+
+func WithPrefixEnums(b bool) Option {
+	return option.New(optkeyPrefixEnums, b)
 }
